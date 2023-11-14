9819d6d794546cc7352047d265761afd00415420
Michele-Medeiros <99910808+Michele-Medeiros@users.noreply.github.com>
2023-11-14 08:58:47 -0300

    add space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-    protect_from_forgery with::exception
+    protect_from_forgery with: :exception
     def not_found_method
       render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
     end
-  end
-  
+end
+
