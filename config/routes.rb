69b4e59307a8d91e9e9152636536fc6f75f87ef8
Michele-Medeiros <99910808+Michele-Medeiros@users.noreply.github.com>
2023-11-14 09:10:08 -0300

    again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root :to => 'UsersController#index'
+  root :to => 'users#index'
   # resources :users
   resources :users do
     collection do
